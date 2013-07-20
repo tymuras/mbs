b1aaba522874fcd89f66306a5659871494e1c9b0
tymuras <tomas.bruzgelevicius@gmail.com>
2013-07-20 16:44:37 +0300

    Added views

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,16 +1,35 @@
 <?php
 
 class PostsController extends AppController {
-    public $helpers = array('Html', 'Form');
-	public $uses = false;   
+    public $helpers = array('Html', 'Form', 'Session');
+    public $components = array('Session');
 
     public function index() {
-       
-		$this->set('posts', $_SERVER);
+        $this->set('posts', $this->Post->find('all'));
     }
-	
-	 public function view($id = null) {
-          $this->set('post', array('a'=>$id ));
+
+    public function view($id) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Post->findById($id);
+        if (!$post) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+        $this->set('post', $post);
+    }
+
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Post->create();
+            if ($this->Post->save($this->request->data)) {
+                $this->Session->setFlash(__('Your post has been saved.'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('Unable to add your post.'));
+            }
+        }
     }
 }
 ?>
--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,6 +1,12 @@
 <?php
 	
-	class Post extends AppModel {
-		
-		public $useTable = false;
+class Post extends AppModel {
+    public $validate = array(
+        'title' => array(
+            'rule' => 'notEmpty'
+        ),
+        'body' => array(
+            'rule' => 'notEmpty'
+        )
+    );
 }
